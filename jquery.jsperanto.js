ce83f1fb9eff14e530f24bb84d95cf2d3a385880
jpjoyal <jpjoyal@gmail.com>
2012-06-27 10:55:51 -0400

    Enabled sync init via options "async", added corresponding tests
    Default are now reapplied for every init as previous init options were
    leaking into subsequent init calls

--- jquery.jsperanto.js
+++ jquery.jsperanto.js
@@ -2,150 +2,152 @@
 
 (function($) {
    
-	//defaults
-	
-	var o = {};
-    o.interpolationPrefix = '__';
-    o.interpolationSuffix = '__';
-    o.pluralSuffix = '_plural';
-    o.getSuffixMethod = function(count){ return ( count > 1 || typeof(count) == "string" )  ? o.pluralSuffix : ""; };
-    o.maxRecursion = 50; //used while applying reuse of strings to avoid infinite loop
-    o.reusePrefix = "$t(";
-    o.reuseSuffix = ")";
-    o.fallbackLang = 'en-US'; // see Language fallback section
-    o.dicoPath = 'locales'; // see Dictionary section
-    o.keyseparator = "."; // keys passed to $.jsperanto.translate use this separator
-    o.setDollarT = true; // $.t aliases $.jsperanto.translate, nice shortcut
-    o.dictionary = false; // to supply the dictionary instead of loading it using $.ajax. A (big) javascript object containing your namespaced translations
-	o.lang = false; //specify a language to use
-	o.suffixNotFound = ["suffix_not_found_", Math.random()].join(''); // used internally by translate
-	
-	var dictionary = false; //not yet loaded
-	var currentLang = false;
-	var count_of_replacement = 0;
+    
+    var o = {};
+    var dictionary = false; //not yet loaded
+    var currentLang = false;
+    var count_of_replacement = 0;
 
-	
-	function init(callback,options){
-		$.extend(o,options);
-		if(!o.lang){o.lang = detectLanguage();}
-		loadDictionary(o.lang,function(loadedLang){
-			currentLang = loadedLang;
-			if(o.setDollarT){$.t = $.t || translate;} //shortcut
-			callback(translate);
-		});
-	}
+    
+    function init(callback,options){
+        $.extend(o,{
+            //defaults
+            interpolationPrefix : '__',
+            interpolationSuffix : '__',
+            pluralSuffix : '_plural',
+            getSuffixMethod : function(count){ return ( count > 1 || typeof(count) == "string" )  ? o.pluralSuffix : ""; },
+            maxRecursion : 50, //used while applying reuse of strings to avoid infinite loop
+            reusePrefix : "$t(",
+            reuseSuffix : ")",
+            fallbackLang : 'en-US', // see Language fallback section
+            dicoPath : 'locales', // see Dictionary section
+            async : true, // might be used to force initialization, dictionary loading to be syncronous
+            keyseparator : ".", // keys passed to $.jsperanttranslate use this separator
+            setDollarT : true, // $.t aliases $.jsperanttranslate, nice shortcut
+            dictionary : false, // to supply the dictionary instead of loading it using $.ajax. A (big) javascript object containing your namespaced translations
+            lang : false, //specify a language to use
+            suffixNotFound : ["suffix_not_found_", Math.random()].join('') // used internally by translate
+        },options);
+        if(!o.lang){o.lang = detectLanguage();}
+        loadDictionary(o.lang,function(loadedLang){
+            currentLang = loadedLang;
+            if(o.setDollarT){$.t = $.t || translate;} //shortcut
+            callback(translate);
+        });
+    }
 
-	function applyReplacement(string,replacementHash){
-		$.each(replacementHash,function(key,value){
-			string = string.replace([o.interpolationPrefix,key,o.interpolationSuffix].join(''),value);
-		});
-		return string;
-	}
-	
-	function applyReuse(translated,options){
-		while (translated.indexOf(o.reusePrefix) != -1){
-			count_of_replacement++;
-			if(count_of_replacement > o.maxRecursion){break;} // safety net for too much recursion
-			var index_of_opening = translated.indexOf(o.reusePrefix);
-			var index_of_end_of_closing = translated.indexOf(o.reuseSuffix,index_of_opening) + o.reuseSuffix.length;
-			var token = translated.substring(index_of_opening,index_of_end_of_closing);
-			var token_sans_symbols = token.replace(o.reusePrefix,"").replace(o.reuseSuffix,"");
-			var translated_token = _translate(token_sans_symbols,options);
-			translated = translated.replace(token,translated_token);
-		}
-		return translated;
-	}
-	
-	function detectLanguage(){
-		if(navigator){
-			return (navigator.language) ? navigator.language : navigator.userLanguage;
-		}else{
-			return o.fallbackLang;
-		}
-	}
+    function applyReplacement(string,replacementHash){
+        $.each(replacementHash,function(key,value){
+            string = string.replace([o.interpolationPrefix,key,o.interpolationSuffix].join(''),value);
+        });
+        return string;
+    }
+    
+    function applyReuse(translated,options){
+        while (translated.indexOf(o.reusePrefix) != -1){
+            count_of_replacement++;
+            if(count_of_replacement > o.maxRecursion){break;} // safety net for too much recursion
+            var index_of_opening = translated.indexOf(o.reusePrefix);
+            var index_of_end_of_closing = translated.indexOf(o.reuseSuffix,index_of_opening) + o.reuseSuffix.length;
+            var token = translated.substring(index_of_opening,index_of_end_of_closing);
+            var token_sans_symbols = token.replace(o.reusePrefix,"").replace(o.reuseSuffix,"");
+            var translated_token = _translate(token_sans_symbols,options);
+            translated = translated.replace(token,translated_token);
+        }
+        return translated;
+    }
+    
+    function detectLanguage(){
+        if(navigator){
+            return (navigator.language) ? navigator.language : navigator.userLanguage;
+        }else{
+            return o.fallbackLang;
+        }
+    }
 
-	function containsCount(options){
-	   return (typeof options.count == 'number' || typeof options.count == 'string');
-	}
+    function containsCount(options){
+       return (typeof options.count == 'number' || typeof options.count == 'string');
+    }
 
-	function getCountSuffix(options) {
-	   var suffix = o.getSuffixMethod(options.count);
-	   return ( typeof(suffix) == "string" ) ? suffix : '';
+    function getCountSuffix(options) {
+       var suffix = o.getSuffixMethod(options.count);
+       return ( typeof(suffix) == "string" ) ? suffix : '';
    }
 
-	function translate(dottedkey,options){
-		count_of_replacement = 0;
-		return _translate(dottedkey,options);
-	}
+    function translate(dottedkey,options){
+        count_of_replacement = 0;
+        return _translate(dottedkey,options);
+    }
 
-	/*
-	options.defaultValue
-	options.count
-	*/
-	function _translate(dottedkey,options){
-		options = options || {};
-		var notfound = options.defaultValue || dottedkey;
-		if(!dictionary){return notfound;} // No dictionary to translate from
+    /*
+    options.defaultValue
+    options.count
+    */
+    function _translate(dottedkey,options){
+        options = options || {};
+        var notfound = options.defaultValue || dottedkey;
+        if(!dictionary){return notfound;} // No dictionary to translate from
 
-		if(containsCount(options)){
-			var optionsSansCount = $.extend({},options);
-			delete optionsSansCount.count;
-			optionsSansCount.defaultValue = o.suffixNotFound;
-			var suffixKey = dottedkey + getCountSuffix(options);
-			var translated = translate(suffixKey,optionsSansCount);
-			if(translated != o.suffixNotFound){
-				return applyReplacement(translated,{count:options.count});//apply replacement for count only
-			}// else continue translation with original/singular key
-		}
-		
-		var keys = dottedkey.split(o.keyseparator);
-		var i = 0;
-		var value = dictionary;
+        if(containsCount(options)){
+            var optionsSansCount = $.extend({},options);
+            delete optionsSansCount.count;
+            optionsSansCount.defaultValue = o.suffixNotFound;
+            var suffixKey = dottedkey + getCountSuffix(options);
+            var translated = translate(suffixKey,optionsSansCount);
+            if(translated != o.suffixNotFound){
+                return applyReplacement(translated,{count:options.count});//apply replacement for count only
+            }// else continue translation with original/singular key
+        }
+        
+        var keys = dottedkey.split(o.keyseparator);
+        var i = 0;
+        var value = dictionary;
         while(keys[i]) {
             value = value && value[keys[i]];
             i++;
         }
-		if(value){
-			value = applyReplacement(value,options);
-			value = applyReuse(value,options);
-			return value;
-		}else{
-			return notfound;
-		}
-	}
-	
-	function loadDictionary(lang,doneCallback){
-		if(o.dictionary){
-			dictionary = o.dictionary;
-			doneCallback(lang);
-			return;
-		}
-		$.ajax({
-			url: [o.dicoPath,"/", lang, '.json'].join(''),
-			success: function(data,status,xhr){
-				dictionary = data;
-				doneCallback(lang);
-			},
-			error : function(xhr,status,error){
-				if(lang != o.fallbackLang){
-					loadDictionary(o.fallbackLang,doneCallback);
-				}else{
-					doneCallback(false);
-				}
-			},
-			dataType: "json"
-		});
-	}
-	
-	function lang(){
-		return currentLang;
-	}
+        if(value){
+            value = applyReplacement(value,options);
+            value = applyReuse(value,options);
+            return value;
+        }else{
+            return notfound;
+        }
+    }
+    
+    function loadDictionary(lang,doneCallback){
+        if(o.dictionary){
+            dictionary = o.dictionary;
+            doneCallback(lang);
+            return;
+        }
+        $.ajax({
+            url: [o.dicoPath,"/", lang, '.json'].join(''),
+            success: function(data,status,xhr){
+                dictionary = data;
+                doneCallback(lang);
+            },
+            error : function(xhr,status,error){
+                if(lang != o.fallbackLang){
+                    loadDictionary(o.fallbackLang,doneCallback);
+                }else{
+                    doneCallback(false);
+                }
+            },
+            async : o.async,
+            dataType: "json"
+        });
+    }
+    
+    function lang(){
+        return currentLang;
+    }
 
-	$.jsperanto = $.jsperanto || {
-		init:init,
-		t:translate,
-		translate:translate,
-		detectLanguage : detectLanguage,
-		lang : lang
-	};
+    $.jsperanto = $.jsperanto || {
+        init:init,
+        t:translate,
+        translate:translate,
+        detectLanguage : detectLanguage,
+        lang : lang
+    };
 })(jQuery);
--- test/test.js
+++ test/test.js
@@ -1,8 +1,44 @@
-asyncTest("translation", function() {
+function resetDollarT(){
+    $.t = null;
+}
+test("sync init", function() {
     var o = {
-      lang : 'testlang'
-   };
-   $.jsperanto.init(function(t){
+        lang : 'testlang',
+        async:false
+    };
+    var arewesync = false;
+    $.jsperanto.init(function(){
+        arewesync = true;
+    },o);
+    ok(arewesync,"init is syncronous");
+    equals($.t('product.name'),"jsperanto");
+    resetDollarT();
+});
+
+test("async init", function() {
+    var o = {
+        lang : 'testlang'
+    };
+    var arewesync = false;
+    $.jsperanto.init(function(){
+        arewesync = true;
+        resetDollarT();
+    },o);
+    ok(arewesync === false,"init is asyncronous");
+    try{
+        $.t('product.name');
+    }catch(e){
+        ok(true,"$.t does not exist");
+    }
+});
+
+test("translation", function() {
+    expect(28);
+    var o = {
+        lang : 'testlang'
+    };
+    stop();
+    $.jsperanto.init(function(t){
         equals(t('product.name'),"jsperanto");
         equals(t('withreuse'),"jsperanto and Home");
         equals(t('withreplacement',{year:2005}),"since 2005");
@@ -18,41 +54,42 @@ asyncTest("translation", function() {
         equals(t('count and replacement',{count:1}),"you have 1 friend");
         equals(t('count and replacement',{count:3}),"you have 3 friends");
 
-        
+        equals($.t('can_speak_plural',{count:'any'}),"I can speak any languages","count can be a string");
 
-      equals($.t('can_speak_plural',{count:'any'}),"I can speak any languages","count can be a string");
-
-      equals($.t('project.size.source',{value:4,unit:"kb"}),"jsperanto is 4 kb","Interpolation variables can be a number");
-      equals($.t('project.size.min',{value:1010,unit:"bytes"}),"jsperanto is 1010 bytes when minified","options are also used for nested lookup ");
-      equals($.t('project.size.gzip',{value:505,unit:"bytes"}),"jsperanto is 505 bytes when minified and gzipped","options are also used for nested lookup");
-
+        equals($.t('project.size.source',{value:4,unit:"kb"}),"jsperanto is 4 kb","Interpolation variables can be a number");
+        equals($.t('project.size.min',{value:1010,unit:"bytes"}),"jsperanto is 1010 bytes when minified","options are also used for nested lookup ");
+        equals($.t('project.size.gzip',{value:505,unit:"bytes"}),"jsperanto is 505 bytes when minified and gzipped","options are also used for nested lookup");
         equals(t('not.existing.key'),"not.existing.key");
-
-      t('infinite');
-      equals(true,true,"recursive nested lookup should not crash");
-
+        t('infinite');
+        equals(true,true,"recursive nested lookup should not crash");
+        resetDollarT();
     },o);
-
-    o.getSuffixMethod = function(count){
-       if ( count == 0 ) {
-          return "_zero";
-       }
-       if ( count != 1 ) {
-          return "_plural";
-       }
+    
+    o = {
+        lang : 'testlang',
+        getSuffixMethod : function(count){
+            if ( count == 0 ) {
+                return "_zero";
+            }
+            if ( count != 1 ) {
+                return "_plural";
+            }
+        }
     };
     $.jsperanto.init(function(t){
-      equals(t('countCustomSuffix',{count:0}),"I have exactly zero. The count is 0");
-      equals(t('countCustomSuffix',{count:0.5}),"I have many. The count is 0.5");
-      equals(t('countCustomSuffix',{count:1}),"I have exactly one. The count is 1");
-      equals(t('countCustomSuffix',{count:3}),"I have many. The count is 3");
-      equals(t('countCustomSuffix',{count:-1}),"I have many. The count is -1");
-      equals(t('countCustomSuffix',{count:"0"}),"I have exactly zero. The count is 0");
-      equals(t('countCustomSuffix',{count:"string"}),"I have many. The count is string");
-      equals(t('countCustomSuffix',{count:"1"}),"I have exactly one. The count is 1");
-      equals(t('countCustomSuffix',{count:"01"}),"I have exactly one. The count is 01");
-      start();
+        equals(t('countCustomSuffix',{count:0}),"I have exactly zero. The count is 0");
+        equals(t('countCustomSuffix',{count:0.5}),"I have many. The count is 0.5");
+        equals(t('countCustomSuffix',{count:1}),"I have exactly one. The count is 1");
+        equals(t('countCustomSuffix',{count:3}),"I have many. The count is 3");
+        equals(t('countCustomSuffix',{count:-1}),"I have many. The count is -1");
+        equals(t('countCustomSuffix',{count:"0"}),"I have exactly zero. The count is 0");
+        equals(t('countCustomSuffix',{count:"string"}),"I have many. The count is string");
+        equals(t('countCustomSuffix',{count:"1"}),"I have exactly one. The count is 1");
+        equals(t('countCustomSuffix',{count:"01"}),"I have exactly one. The count is 01");
+        start();
+        resetDollarT();
     },o);
 });
 
-//TODO test options
+
+//TODO test more init options
